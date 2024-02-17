c5e255d62bc4fb5d9834d8a88a7915fc1df74433
Alisher250 <81162609+Alisher250@users.noreply.github.com>
2024-02-17 20:40:46 +0600

    Update settings.py

--- birge/settings.py
+++ birge/settings.py
@@ -123,14 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = BASE_DIR / 'static/main/images'
+STATIC_ROOT = BASE_DIR / "static"
+MEDIA_ROOT = BASE_DIR / 'main/static/main/images'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
