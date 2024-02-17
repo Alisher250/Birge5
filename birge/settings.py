8dad63fe38b3e6fd8f70887427be49fb12a6e5ac
Alisher250 <81162609+Alisher250@users.noreply.github.com>
2024-02-17 21:45:19 +0600

    Update settings.py

--- birge/settings.py
+++ birge/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATIC_ROOT = BASE_DIR / "static"
-MEDIA_ROOT = BASE_DIR / 'static/main/images'
+MEDIA_ROOT = BASE_DIR / 'main/static/main/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
