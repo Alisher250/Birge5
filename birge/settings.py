2d5cdba66dbbf7f8ec9e87f3d07c72e618c09e87
Alisher250 <81162609+Alisher250@users.noreply.github.com>
2024-02-17 20:45:22 +0600

    Update settings.py

--- birge/settings.py
+++ birge/settings.py
@@ -128,8 +128,6 @@ MEDIA_URL = '/images/'
 STATIC_ROOT = BASE_DIR / "static"
 MEDIA_ROOT = BASE_DIR / 'main/static/main/images'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
