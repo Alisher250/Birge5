4585e33c9218e9143bc3793744a028f8e78c1309
Alisher250 <81162609+Alisher250@users.noreply.github.com>
2024-02-17 19:52:22 +0600

    Update settings.py

--- birge/settings.py
+++ birge/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-vyp9=t+a(a03!oddkqm7e0(m=+$mlresv=t9dcfmd4ad$o^^!=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*','https://web-production-5af3.up.railway.app/']
 
 
 # Application definition
